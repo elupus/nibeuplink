6f17ac9d4f50e5f2de5bc38c242fdb7dfdd0709d
Joakim Plate <joakim.plate@gmail.com>
2017-11-08 22:17:20 +0100

    Add some system and parameter functions

--- nibeuplink/__init__.py
+++ nibeuplink/__init__.py
@@ -2,6 +2,7 @@ import logging
 import sys
 import os
 import pickle
+from itertools import islice
 
 from requests_oauthlib import OAuth2Session
 
@@ -14,6 +15,11 @@ TOKEN_URL           = '%s/oauth/token' % BASE_URL
 AUTH_URL            = '%s/oauth/authorize' % BASE_URL
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+def chunks(data, SIZE):
+    it = iter(data)
+    for i in range(0, len(data), SIZE):
+        yield {k:data[k] for k in islice(it, SIZE)}
 
 class OAuth2(OAuth2Session):
     def __init__(self, client_id, client_secret, redirect_uri, token, token_updater):
@@ -45,13 +51,71 @@ class OAuth2(OAuth2Session):
         return token
 
 
+class System():
+    def __init__(self, uplink, system_id):
+        self.uplink     = uplink
+        self.system_id  = system_id
+        self.parameters = {}
+        self.data       = None
+
+class Parameter():
+    def __init__(self, uplink, system_id, parameter_id):
+        self.uplink       = uplink
+        self.system_id    = system_id
+        self.parameter_id = parameter_id
+        self.data         = None
 
 class Uplink():
 
     def __init__(self, session):
-        self.session         = session
+        self.session    = session
+        self.systems    = {}
 
+    def get(self, uri, params = {}):
+        if not self.session.authorized:
+            return None
 
+        headers = {}
+        url = '%s/api/v1/%s' % (BASE_URL, uri)
+        data = self.session.get(url, params=params, headers=headers).json()
+        _LOGGER.debug(data)
+        return data
 
+    def update(self):
+        for system_id in self.systems.keys():
+            self.update_parameters(system_id)
 
+    def update_parameters(self, system_id):
+            system = self.get_system(system_id)
+            for parameters in chunks(system.parameters, MAX_REQUEST_PARAMETERS):
+                _LOGGER.debug(parameters)
 
+                data = self.get(
+                            'systems/{}/parameters'.format(system.system_id),
+                            { 'parameterIds' : parameters.keys() }
+                       )
+
+                for parameter in data:
+                    p = self.get_parameter(system.system_id, parameter['parameterId'])
+                    p.data = data
+
+    def get_system(self, system_id):
+        system = None
+        if system_id in self.systems:
+            system = self.systems[system_id]
+        else:
+            system = System(self, system_id)
+            self.systems[system_id] = system
+        return system
+
+    def get_parameter(self, system_id, parameter_id):
+        system    = self.get_system(system_id)
+        parameter = None
+
+        if parameter_id in system.parameters:
+            parameter = system.parameters[parameter_id]
+        else:
+            parameter = Parameter(self, system_id, parameter_id)
+            system.parameters[parameter_id] = parameter
+
+        return parameter
--- tests/console.py
+++ tests/console.py
@@ -1,10 +1,22 @@
-import nibeuplink
-import argparse
 import logging
 import sys
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+
+import nibeuplink
+import argparse
 import pickle
 
 _LOGGER = logging.getLogger(__name__)
+
 
 parser = argparse.ArgumentParser(description='Read data from nibe uplink.')
 parser.add_argument('client_id')
@@ -29,7 +41,7 @@ def token_write(token):
     with open(STORE, 'wb') as myfile:
         pickle.dump(token, myfile)
 
-print(token_read())
+_LOGGER.debug(token_read())
 oauth2 = nibeuplink.OAuth2(args.client_id, args.client_secret, args.redirect_uri, token_read(), token_write)
 if not oauth2.authorized:
 	auth_uri, state = oauth2.authorization_url()
@@ -38,3 +50,10 @@ if not oauth2.authorized:
 
 	token = oauth2.fetch_token(result)
 	print(token)
+
+
+uplink = nibeuplink.Uplink(oauth2)
+uplink.get_parameter(36563, '43424')
+uplink.get_parameter(36563, '40032')
+uplink.update()
+print(uplink.get_parameter(36563, '43424'))
