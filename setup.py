9118dabed9933e30aebe12c935426e02252b00a3
Joakim Plate <joakim.plate@gmail.com>
2017-11-08 22:14:14 +0100

    Switch to spaces

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='nibe-uplink',
-	version='0.1',
-	description='A python wrapper around Nibe Uplink REST API',
-	license='MIT',
-	packages=find_packages(exclude=['contrib', 'docs', 'tests'])
+    name='nibeuplink',
+    version='0.1',
+    description='A python wrapper around Nibe Uplink REST API',
+    license='MIT',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    python_requires='>3.5',
 )
