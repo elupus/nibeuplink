368e646768d3ec571537aabd7a6b3951604b0ce4
Joakim Plate <joakim.plate@gmail.com>
2017-12-25 15:13:00 +0100

    Bump version to 0.2.0 to differentiate with changes to value

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nibeuplink',
-    version='0.1',
+    version='0.2.0',
     description='A python wrapper around Nibe Uplink REST API',
     license='MIT',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
