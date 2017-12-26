394214dd0c146973d208e71807c6165ed089e874
Joakim Plate <elupus@ecce.se>
2017-12-26 18:46:12 +0100

    Bump version for changes to notification function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nibeuplink',
-    version='0.2.0',
+    version='0.3.0',
     description='A python wrapper around Nibe Uplink REST API',
     license='MIT',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
