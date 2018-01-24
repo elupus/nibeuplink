089ccc507c70c9bb8532b6b6278a13d00eb264aa
Joakim Plate <joakim.plate@gmail.com>
2018-01-24 19:20:34 +0100

    Adjust indentation for tests

--- tests/tests.py
+++ tests/tests.py
@@ -50,36 +50,37 @@ class TestUnauthenticated(AioTestCase):
         self.uplink = None
 
     async def test_status(self):
-            self.assertFalse(self.uplink.access_data)
+        self.assertFalse(self.uplink.access_data)
 
     async def test_get_authorize_url(self):
-            from urllib.parse import (urlsplit, parse_qs)
+        from urllib.parse import (urlsplit, parse_qs)
 
-            o = urlsplit(self.uplink.get_authorize_url())
-            q = parse_qs(o.query)
+        o = urlsplit(self.uplink.get_authorize_url())
+        q = parse_qs(o.query)
 
-            self.assertEqual(q['response_type'], ['code'])
-            self.assertEqual(q['client_id'],     [DEFAULT_CLIENT_ID])
-            self.assertEqual(q['redirect_uri'],  [DEFAULT_REDIRECT_URI])
-            self.assertEqual(q['scope'],         [' '.join(DEFAULT_SCOPE)])
+        self.assertEqual(q['response_type'], ['code'])
+        self.assertEqual(q['client_id'],     [DEFAULT_CLIENT_ID])
+        self.assertEqual(q['redirect_uri'],  [DEFAULT_REDIRECT_URI])
+        self.assertEqual(q['scope'],         [' '.join(DEFAULT_SCOPE)])
 
     async def test_get_code_from_url(self):
-            from urllib.parse import (urlsplit, parse_qs)
-            o = urlsplit(self.uplink.get_authorize_url())
-            q = parse_qs(o.query)
+        from urllib.parse import (urlsplit, parse_qs)
+        o = urlsplit(self.uplink.get_authorize_url())
+        q = parse_qs(o.query)
 
 
-            # valid state
-            url = "{}?state={}&code={}".format(DEFAULT_REDIRECT_URI, q['state'][0], DEFAULT_CODE)
-            self.assertEqual(DEFAULT_CODE, self.uplink.get_code_from_url(url))
+        # valid state
+        url = "{}?state={}&code={}".format(DEFAULT_REDIRECT_URI, q['state'][0], DEFAULT_CODE)
+        self.assertEqual(DEFAULT_CODE, self.uplink.get_code_from_url(url))
 
-            # no state
-            url = "{}?code={}".format(DEFAULT_REDIRECT_URI, DEFAULT_CODE)
-            self.assertEqual(DEFAULT_CODE, self.uplink.get_code_from_url(url))
+        # no state
+        url = "{}?code={}".format(DEFAULT_REDIRECT_URI, DEFAULT_CODE)
+        self.assertEqual(DEFAULT_CODE, self.uplink.get_code_from_url(url))
 
-            # invalid state
-            url = "{}?state={}&code={}".format(DEFAULT_REDIRECT_URI, "junk", DEFAULT_CODE)
-            self.assertRaises(ValueError, self.uplink.get_code_from_url, url)
+        # invalid state
+        url = "{}?state={}&code={}".format(DEFAULT_REDIRECT_URI, "junk", DEFAULT_CODE)
+        self.assertRaises(ValueError, self.uplink.get_code_from_url, url)
+
 
 
 if __name__ == '__main__':
