af66ddf42808d0fefd7182a5d3a4a7e6cd3f98ef
Joakim Plate <elupus@ecce.se>
2018-06-16 14:00:08 +0200

    Bump to version 3.2 in preparation for new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nibeuplink',
-    version='0.3.1',
+    version='0.3.2',
     description='A python wrapper around Nibe Uplink REST API',
     license='MIT',
     packages=find_packages('src'),
