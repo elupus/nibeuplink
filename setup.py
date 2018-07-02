056f6929740490eb05299c96d4f572ecb5bc2f43
Joakim Plate <joakim.plate@gmail.com>
2018-07-02 23:58:27 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nibeuplink',
-    version='0.4.2',
+    version='0.4.3',
     description='A python wrapper around Nibe Uplink REST API',
     license='MIT',
     packages=find_packages('src'),
