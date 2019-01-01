4269011b5c77e32ff6f2527a565b4ae432a18cc7
Joakim Plate <elupus@ecce.se>
2019-01-01 17:49:22 +0100

    Bump nibe uplink version to 0.5.0 for addition of water heater

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nibeuplink',
-    version='0.4.3',
+    version='0.5.0',
     description='A python wrapper around Nibe Uplink REST API',
     license='MIT',
     packages=find_packages('src'),
