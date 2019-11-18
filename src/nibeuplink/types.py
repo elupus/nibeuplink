5624ed344e270ac9bb2060d18a98d3f679aad181
Joakim Plate <elupus@ecce.se>
2019-11-18 21:43:32 +0100

    Switch to non runtime type hints since we dropped 3.5 support

--- src/nibeuplink/types.py
+++ src/nibeuplink/types.py
@@ -28,51 +28,51 @@ class ParameterExtended(Parameter):
 @attr.s
 class ClimateSystem(object):
     name = attr.ib(type=str)
-    return_temp = attr.ib(type=Optional[ParameterId])  # BT3
-    supply_temp = attr.ib(type=Optional[ParameterId])  # BT2
-    calc_supply_temp_heat = attr.ib(type=Optional[ParameterId])  # CSTH
-    offset_cool = attr.ib(type=Optional[ParameterId])  # OC
-    room_temp = attr.ib(type=Optional[ParameterId])  # BT50
-    room_setpoint_heat = attr.ib(type=Optional[ParameterId])  # RSH
-    room_setpoint_cool = attr.ib(type=Optional[ParameterId])  # RSC
-    use_room_sensor = attr.ib(type=Optional[ParameterId])  # URS
-    active_accessory = attr.ib(type=Optional[ParameterId])  # AA
-    external_adjustment_active = attr.ib(type=Optional[ParameterId])  # EAA
-    calc_supply_temp_cool = attr.ib(type=Optional[ParameterId])  # CSTC
-    offset_heat = attr.ib(type=Optional[ParameterId])  # OH
-    heat_curve = attr.ib(type=Optional[ParameterId])  # HC
-    min_supply = attr.ib(type=Optional[ParameterId])  # MIS
-    max_supply = attr.ib(type=Optional[ParameterId])  # MAS
-    extra_heat_pump = attr.ib(type=Optional[ParameterId])  # EHP
+    return_temp: Optional[ParameterId] = attr.ib()  # BT3
+    supply_temp: Optional[ParameterId] = attr.ib()  # BT2
+    calc_supply_temp_heat: Optional[ParameterId] = attr.ib()  # CSTH
+    offset_cool: Optional[ParameterId] = attr.ib()  # OC
+    room_temp: Optional[ParameterId] = attr.ib()  # BT50
+    room_setpoint_heat: Optional[ParameterId] = attr.ib()  # RSH
+    room_setpoint_cool: Optional[ParameterId] = attr.ib()  # RSC
+    use_room_sensor: Optional[ParameterId] = attr.ib()  # URS
+    active_accessory: Optional[ParameterId] = attr.ib()  # AA
+    external_adjustment_active: Optional[ParameterId] = attr.ib()  # EAA
+    calc_supply_temp_cool: Optional[ParameterId] = attr.ib()  # CSTC
+    offset_heat: Optional[ParameterId] = attr.ib()  # OH
+    heat_curve: Optional[ParameterId] = attr.ib()  # HC
+    min_supply: Optional[ParameterId] = attr.ib()  # MIS
+    max_supply: Optional[ParameterId] = attr.ib()  # MAS
+    extra_heat_pump: Optional[ParameterId] = attr.ib()  # EHP
 
 
 @attr.s
 class HotWaterSystem(object):
     name = attr.ib(type=str)
-    hot_water_charging = attr.ib(type=Optional[ParameterId])  # BT6
-    hot_water_top = attr.ib(type=Optional[ParameterId])  # BT7
-    hot_water_comfort_mode = attr.ib(type=Optional[ParameterId])
-    hot_water_production = attr.ib(type=Optional[ParameterId])  # Active or not
-    periodic_hot_water = attr.ib(type=Optional[ParameterId])
-    stop_temperature_water_normal = attr.ib(type=Optional[ParameterId])
-    start_temperature_water_normal = attr.ib(type=Optional[ParameterId])
-    stop_temperature_water_luxary = attr.ib(type=Optional[ParameterId])
-    start_temperature_water_luxary = attr.ib(type=Optional[ParameterId])
-    stop_temperature_water_economy = attr.ib(type=Optional[ParameterId])
-    start_temperature_water_economy = attr.ib(type=Optional[ParameterId])
-    total_hot_water_compressor_time = attr.ib(type=Optional[ParameterId])
-    hot_water_boost = attr.ib(type=Optional[ParameterId])
+    hot_water_charging: Optional[ParameterId] = attr.ib()  # BT6
+    hot_water_top: Optional[ParameterId] = attr.ib()  # BT7
+    hot_water_comfort_mode: Optional[ParameterId] = attr.ib()
+    hot_water_production: Optional[ParameterId] = attr.ib()  # Active or not
+    periodic_hot_water: Optional[ParameterId] = attr.ib()
+    stop_temperature_water_normal: Optional[ParameterId] = attr.ib()
+    start_temperature_water_normal: Optional[ParameterId] = attr.ib()
+    stop_temperature_water_luxary: Optional[ParameterId] = attr.ib()
+    start_temperature_water_luxary: Optional[ParameterId] = attr.ib()
+    stop_temperature_water_economy: Optional[ParameterId] = attr.ib()
+    start_temperature_water_economy: Optional[ParameterId] = attr.ib()
+    total_hot_water_compressor_time: Optional[ParameterId] = attr.ib()
+    hot_water_boost: Optional[ParameterId] = attr.ib()
 
 
 @attr.s
 class VentilationSystem(object):
     name = attr.ib(type=str)
-    fan_speed = attr.ib(type=Optional[ParameterId])
-    exhaust_air = attr.ib(type=Optional[ParameterId])  # BT20
-    extract_air = attr.ib(type=Optional[ParameterId])  # BT21
-    exhaust_speed_normal = attr.ib(type=Optional[ParameterId])
-    exhaust_speed_1 = attr.ib(type=Optional[ParameterId])
-    exhaust_speed_2 = attr.ib(type=Optional[ParameterId])
-    exhaust_speed_3 = attr.ib(type=Optional[ParameterId])
-    exhaust_speed_4 = attr.ib(type=Optional[ParameterId])
-    ventilation_boost = attr.ib(type=Optional[ParameterId])
+    fan_speed: Optional[ParameterId] = attr.ib()
+    exhaust_air: Optional[ParameterId] = attr.ib()  # BT20
+    extract_air: Optional[ParameterId] = attr.ib()  # BT21
+    exhaust_speed_normal: Optional[ParameterId] = attr.ib()
+    exhaust_speed_1: Optional[ParameterId] = attr.ib()
+    exhaust_speed_2: Optional[ParameterId] = attr.ib()
+    exhaust_speed_3: Optional[ParameterId] = attr.ib()
+    exhaust_speed_4: Optional[ParameterId] = attr.ib()
+    ventilation_boost: Optional[ParameterId] = attr.ib()
